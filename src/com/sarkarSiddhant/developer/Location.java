d5971ed9e11fab99cdd933ebc10105c11b2cb964
Siddhant Sarkar <siddhant.sarkar999@gmail.com>
2020-09-27 09:12:33 +0530

    NUll pointer exception fixed

--- src/com/sarkarSiddhant/developer/Location.java
+++ src/com/sarkarSiddhant/developer/Location.java
@@ -14,13 +14,15 @@ public class Location {
     public Location(int locationID, String description,Map<String,Integer>exits) {
         this.locationID = locationID;
         this.description = description;
-        this.exits = new HashMap<>(exits);
+        if (exits!=null){
+            this.exits = new HashMap<>(exits);
+
+        } else{
+            this.exits=new HashMap<>();
+        }
         this.exits.put("Q", 0);
     }
 
-//    public void addExit(String direction, int location) {
-//        exits.put(direction, location);
-//    }
     public int getLocationID() {
         return locationID;
     }
